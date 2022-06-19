0ff41b8a7d7d32f63f43af4ea14190d04cd730a1
KenHaya <hkearth777@gmail.com>
2022-06-19 15:14:51 +0900

    プロフィール機能作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string(255)
+#  self_introduction      :text(65535)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_105331) do
+ActiveRecord::Schema.define(version: 2022_06_16_132518) do
 
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_06_16_105331) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "avatar"
+    t.text "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
